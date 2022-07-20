08db98703a527440ab08d00f9fc2b0008832a4b3
Vil Harvey <vharvey@apple.com>
2022-07-20 01:17:05 +0100

    Add header guards

--- miniply.h
+++ miniply.h
@@ -22,6 +22,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#ifndef MINIPLY_H
+#define MINIPLY_H
+
 #include <cstdarg>
 #include <cstdint>
 #include <cstdio>
@@ -310,3 +313,5 @@ namespace miniply {
   uint32_t triangulate_polygon(uint32_t n, const float pos[], uint32_t numVerts, const int indices[], int dst[]);
 
 } // namespace miniply
+
+#endif // MINIPLY_H
